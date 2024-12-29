b76f80641823d36e0f8aa4c6c393e386f30c85ce
leone3333 <leonegoveia1@gmail.com>
2024-12-28 23:48:36 -0300

    logger alterado 3

--- utils/logger_config.py
+++ utils/logger_config.py
@@ -15,11 +15,14 @@ console_handler = logging.StreamHandler()
 logger.addHandler(console_handler)
 
 try:
-    # Configura o CloudWatch LogHandler com a região explicitamente
+    # Cria uma sessão boto3 com a região configurada
+    session = boto3.Session(region_name='us-east-1')
+    
+    # Configura o CloudWatch LogHandler utilizando a sessão boto3
     cloudwatch_handler = watchtower.CloudWatchLogHandler(
-        log_group="CHAT-RAG-GROUP-5",
-        stream_name="CHAT-RAG-GROUP-5",
-        region_name="us-east-1"  # Região explícita
+        log_group="CHAT-RAG-GROUP-5",  # Nome do grupo de logs no CloudWatch
+        stream_name="CHAT-RAG-GROUP-5",  # Nome do stream de logs
+        boto3_client=session.client('logs')  # Passa o cliente boto3 configurado
     )
     logger.addHandler(cloudwatch_handler)
     logger.info("CloudWatch Logs configurado com sucesso.")
