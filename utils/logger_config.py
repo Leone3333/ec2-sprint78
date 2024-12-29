98abc34f218678ae9d8846485fa91b1bb5537a69
leone3333 <leonegoveia1@gmail.com>
2024-12-28 23:24:54 -0300

    logger alterado1

--- utils/logger_config.py
+++ utils/logger_config.py
@@ -4,8 +4,7 @@ import watchtower
 from botocore.exceptions import NoCredentialsError
 from utils.env_func import env_func
 
-
-bucket_name, profile_user_name = env_func() 
+bucket_name, profile_user_name = env_func()
 
 # Configura o logger
 logger = logging.getLogger()
@@ -16,15 +15,17 @@ console_handler = logging.StreamHandler()
 logger.addHandler(console_handler)
 
 try:
-    # Cria uma sessão boto3 com o perfil configurado
+    # Cria uma sessão boto3 com a região configurada
     session = boto3.Session(region_name='us-east-1')
     
-    # Configura o CloudWatch LogHandler
+    # Configura o CloudWatch LogHandler com a região
     cloudwatch_handler = watchtower.CloudWatchLogHandler(
         log_group="CHAT-RAG-GROUP-5",  # Nome do grupo de logs no CloudWatch
         stream_name="CHAT-RAG-GROUP-5",  # Nome do stream de logs
+        boto3_session=session  # Passa a sessão boto3 configurada
     )
     logger.addHandler(cloudwatch_handler)
+    logger.info("CloudWatch Logs configurado com sucesso.")
 except NoCredentialsError as e:
     logger.error("Credenciais da AWS não configuradas corretamente.")
     raise e
